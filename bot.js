b4af1d6e4a1bbd60bdc4ba6cb45545208a03acea
Rotem12 <rotem1243@gmail.com>
2022-12-12 22:15:57 +0200

    Adds filters to discord messages
    Filters out links, custom emojis, unicode emojis
    Adds length limits, 18 for name 350 for message.
    Uses cleanContent instead of content so mentions appear with name instead of id.

--- bot.js
+++ bot.js
@@ -20,7 +20,14 @@ client.on('ready', () =>
 client.on('messageCreate', msg => 
 {
 	if(msg.author.bot) return;
-	messageQueue.push(`{"name": "${msg.author.username}", "content": "${msg.content}"}`);
+	
+	let name    = checkString(msg.member.nickname ? msg.member.nickname : msg.author.username, 18);
+	let content = checkString(msg.cleanContent, 350)
+	
+	if(content != "")
+	{
+		messageQueue.push(`{"name": "${name}", "content": "${content}"}`);
+	}
 
 	console.log("The current queue is: ", messageQueue);
 });
@@ -38,45 +45,64 @@ server.listen(port, host, () =>
   console.log(`Server running at http://${host}:${port}/`);
 });
 
+function checkString(s, charLimit=0)
+{
+	// links
+	var pattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+	var pattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+	
+	var pattern3 = /<a?:.+?:\d{18}>|\p{Extended_Pictographic}/gu // unicode emoji
+	var pattern4 = /<a?:.+:\d+>/gm // custom emoji
+	
+    s = s.replace(pattern1, '');
+	s = s.replace(pattern2, '');
+	s = s.replace(pattern3, '');
+	s = s.replace(pattern4, '');
+	
+	if(charLimit > 0)
+	{
+		return s.substring(0, charLimit)
+	}
+	return s
+}
+
 function respondToRequest (request, response)
 {
 	let res = "";
-	if ("content-type" in request["headers"] && request["headers"]["content-type"] == "text/plain" && request.connection.remoteAddress == host)
+	if (!("content-type" in request["headers"]) || request["headers"]["content-type"] != "text/plain" || request.connection.remoteAddress != host)
 	{
-		let body = "";
-		request.on('data', function(chunk)
-		{ 
-			body += chunk;
-		});
+		response.writeHead(400, {"Content-Type": "text/plain"}); 
+		response.end("wrong format");
+		return;
+	}
 	
-		request.on('end', function()
-		{
-			if(body == server_password)
-			{
-				if(messageQueue.length > 0)
-				{
-					res = messageQueue.shift();
-					response.writeHead(200, {"Content-Type": "application/json"}); 
-					response.end(res);
-				} 
-				else
-				{
-					response.writeHead(201, {"Content-Type": "text/plain"}); 
-					response.end("NO");
-				}
-			}
-			else
-			{
-				response.writeHead(401, {"Content-Type": "text/plain"}); 
-				response.end("wrong password");
-			}
-		});
-	}
-	else
+	let body = "";
+	request.on('data', function(chunk)
+	{ 
+		body += chunk;
+	});
+
+	request.on('end', function()
 	{
-			response.writeHead(400, {"Content-Type": "text/plain"}); 
-			response.end("wrong format");
-	}
+		if(body != server_password)
+		{
+			response.writeHead(401, {"Content-Type": "text/plain"}); 
+			response.end("wrong password");
+			return;
+		}
+			
+		if(messageQueue.length > 0)
+		{
+			res = messageQueue.shift();
+			response.writeHead(200, {"Content-Type": "application/json"}); 
+			response.end(res);
+		} 
+		else
+		{
+			response.writeHead(201, {"Content-Type": "text/plain"}); 
+			response.end("NO");
+		}
+	});
 }
 
 
